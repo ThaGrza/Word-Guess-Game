b3648368126e2a6d16fb37acd4b6295758539500
ThaGrza <andrewgriswold7@gmail.com>
2020-06-06 19:25:31 -0500

    update functions for guesses and turns left

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,7 +27,7 @@ var wordGuessGame = {
         this.rebuildWordView();
         this.updateTotalGuesses();
     },
-
+    // Updates page
     updatePage: function(letter) {
         if(this.guessesLeft === 0) {
             this.restartGame();
@@ -41,6 +41,39 @@ var wordGuessGame = {
             }
         }
     },
+    // Wrong guess logic.
+    updateGuesses: function(letter) {
+        if ((this.guessLetters.indexOf(letter) === -1) && (this.lettersOfTheWord.indexOf(letter) === -1)) {
+            // adds letter to guessedLetters array.
+            this.guessedLetters.push(letter);
+            // Subjects 1 from guesses.
+            this.guessesLeft--;
+
+            // Updates guesses and letters.
+            document.querySelector("#guesses-remaining").innerHTML = this.guessesLeft;
+            document.querySelector("#guessed-letters").innerHTML = this.guessedLetters.join(", ");
+        }
+    },
+
+    // Sets initial guesses.
+    processUpdateTotalGuesses: function() {
+        // Increases guesses based on word length.
+        this.totalGuesses = this.lettersOfTheWord.length + 5;
+        this.guessesLeft = this.totalGuesses;
+        
+        // Updates guessesLeft.
+        document.querySelector("#guesses-remaining").innerHTML = this.guessesLeft;
+    },
+
+    // successful Guess logic.
+    updateMatchedLetters: function(letter) {
+        for (var i = 0; i < this.lettersOfTheWord.length; i++) {
+            if ((letter === this.lettersOfTheWord[i]) && (this.matchedLetters.indexOf(letter) === -1)) {
+                this.matchedLetters.push(letter);
+            }
+        }
+    },
 
     
+
 }
